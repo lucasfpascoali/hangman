94f24c7fe75ad7003dd78711ca0f8b2a3fe4d083
Lucas Furlanetto Pascoali <luccafpascoali@gmail.com>
2021-10-15 17:09:13 -0300

    implemented method to get a random word

---
+++ lib/game_logic.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# This module implements methods that are used in the game logic
+module GameLogic
+  MAX_LINES_FILE = 61_406
+
+  def word_from_dictionary
+    dictionary = File.open('5desk.txt', 'r')
+    random_line_number.times { dictionary.gets }
+
+    word = ''
+    loop do
+      word = dictionary.gets.chomp
+      break if word.length.between?(5, 12)
+    end
+    dictionary.close
+    word
+  end
+
+  private
+
+  def random_line_number
+    Random.rand(1..MAX_LINES_FILE) - 1
+  end
+end
